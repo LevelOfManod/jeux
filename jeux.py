12b4cb646d55eda7fc2fb1f1e59988b4a5fa2925
LevelOfManod <vanderlyndenmathieu@gmail.com>
2023-06-13 09:08:04 +0200

    Pierre, feuille ou ciseaux

---
+++ jeux.py
@@ -0,0 +1,20 @@
+import random
+
+options = ["pierre", "papier", "ciseaux"]
+selection_rand = random.choice(options)
+
+choix = input("Sélectionnez pierre, papier ou ciseaux ")
+
+if choix in options:
+    if choix == selection_rand:
+        print("Egalité")
+    else:
+        if (choix == "pierre" and selection_rand == "ciseaux") \
+        or (choix == "papier" and selection_rand == "pierre") \
+        or (choix == "ciseaux" and selection_rand == "papier"):
+            print("Bravo vous avez gagné")
+        else:
+            print("Vous avez perdu")
+    print("Choix de l'ordinateur", selection_rand)
+else: 
+    print("Choix incorrect")
